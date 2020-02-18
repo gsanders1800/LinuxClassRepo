72f5b3118aa4fa9666dd7c23dd366d820362da38
George Sanders <gsanders1600@gmail.com>
2020-02-18 18:35:36 -0500

    C:/Program Files/Git/LinuxClassRepo/Homework/Week04/GeorgeSanders/Problem1/myIfScript.sh

---
+++ myIfScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+Man=100
+Woman=200
+
+#Basic If Statement
+
+if [ "$Man" = 100 ];
+then 
+	echo Man equals 100
+fi
+
+#if else statement
+if [ "$Woman" = 200 ];
+then 
+	echo Woman equals 200
+else 
+	pwd
+fi
